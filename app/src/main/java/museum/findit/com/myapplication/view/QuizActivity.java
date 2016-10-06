8ebf8e089f38a5a698e78f356ef8a2ca92881153
hui <hui8958@gmail.com>
2016-10-06 13:36:13 +0200

    Add Basic UI
    -Add login activity
    -Add Waiting Room activity
    start button only for test
    -Add Game activity
    include Item, leaderboard, quiz

---
+++ app/src/main/java/museum/findit/com/myapplication/view/QuizActivity.java
@@ -0,0 +1,15 @@
+package museum.findit.com.myapplication.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import museum.findit.com.myapplication.R;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
