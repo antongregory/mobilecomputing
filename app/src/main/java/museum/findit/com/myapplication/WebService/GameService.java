42443bdef19a562e332206661968abf3526e0012
Doni Ramadhan <doniramadhan@gmail.com>
2016-10-18 21:36:47 +0200

    add listenScoreboard to update scoreboard in real time

--- app/src/main/java/museum/findit/com/myapplication/WebService/GameService.java
+++ app/src/main/java/museum/findit/com/myapplication/WebService/GameService.java
@@ -1,8 +1,15 @@
 package museum.findit.com.myapplication.WebService;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 /**
  * Created by doniramadhan on 2016-10-12.
@@ -25,5 +32,10 @@ public class GameService {
         DatabaseReference playerDatabase = gamesDatabase.child(gameId).child("players").child(userId);
         playerDatabase.child("score").setValue(score);
         playerDatabase.child("percentage").setValue(percentage);
+    }
+
+    public static void listenScoreboard(ValueEventListener scoreboardListener){
+        DatabaseReference playersDatabase = gamesDatabase.child(gameId).child("players");
+        playersDatabase.addValueEventListener(scoreboardListener);
     }
 }
