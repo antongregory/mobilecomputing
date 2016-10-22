086fbddedb797119aa0a85bf2646d616ea5790f5
hui <hui8958@gmail.com>
2016-10-22 13:54:43 +0200

    -Add QRcode  for sharing the game -Upgrade login UI

--- app/src/main/java/museum/findit/com/myapplication/view/Activities/JoinGameActivity.java
+++ app/src/main/java/museum/findit/com/myapplication/view/Activities/JoinGameActivity.java
@@ -3,8 +3,16 @@ package museum.findit.com.myapplication.view.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.w3c.dom.Text;
 
 import museum.findit.com.myapplication.R;
 import museum.findit.com.myapplication.controller.Controller;
@@ -14,25 +22,22 @@ public class JoinGameActivity extends AppCompatActivity implements Controller.Vi
     String message;
     public final static String EXTRA_MESSAGE_USERNAME = "user_name";
     public final static String EXTRA_MESSAGE_GAMECODE = "gamecode";
-    String gameCode;
+
     public final static String EXTRA_MESSAGE = "name";
-    private EditText editText;
     private Controller mController;
+    TextView welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_game);
-
+        welcome = (TextView) findViewById(R.id.welcomeTxt);
         Intent intent = getIntent();
          message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE_USERNAME);
-        initialise();
+
         mController=new Controller(this);
     }
 
-    private void initialise(){
-        editText=(EditText)findViewById(R.id.gamecodeText);
-    }
 
 
     public void createGameRoom(View view) {
@@ -40,7 +45,55 @@ public class JoinGameActivity extends AppCompatActivity implements Controller.Vi
     }
 
     public void joinGameRoom(View view) {
-        mController.joinGameAction(editText.getText().toString());
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+
+        integrator.setPrompt("Scan");
+
+        integrator.setCameraId(0);
+
+        integrator.setBeepEnabled(false);
+
+        integrator.setBarcodeImageEnabled(false);
+        try{
+            integrator.initiateScan();
+        }
+        catch (Exception e){
+            Log.d("Exception","sa"+e.toString());
+        }
+
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "You cancelled the scanning", Toast.LENGTH_LONG).show();
+                startGame(result.getContents());//test
+
+            }
+            else {
+
+                Toast.makeText(this, result.getContents(),Toast.LENGTH_LONG).show();
+                startGame(result.getContents());
+
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void startGame(String code){
+        mController.joinGameAction(code);
     }
 
     public void onSucess(Class view){
--- app/src/main/java/museum/findit/com/myapplication/view/Activities/WaitingRoomActivity.java
+++ app/src/main/java/museum/findit/com/myapplication/view/Activities/WaitingRoomActivity.java
@@ -1,17 +1,28 @@
 package museum.findit.com.myapplication.view.Activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 import museum.findit.com.myapplication.R;
 import museum.findit.com.myapplication.controller.TimerService;
 import museum.findit.com.myapplication.controller.Controller;
 import museum.findit.com.myapplication.view.Activities.GameActiviry;
 import museum.findit.com.myapplication.view.Activities.LoginActivity;
+
+import static android.R.attr.width;
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
 
 public class WaitingRoomActivity extends AppCompatActivity implements Controller.ViewHandler {
 
@@ -39,7 +50,7 @@ public class WaitingRoomActivity extends AppCompatActivity implements Controller
         }
 
 
-        initialise();
+        initialise("hahahaha");
         mController=new Controller(this);
 
         welcomeInfo.setText("Welcome "+name+"!");
@@ -56,10 +67,44 @@ public class WaitingRoomActivity extends AppCompatActivity implements Controller
         return super.dispatchKeyEvent(event);
     }
 
-    private void initialise(){
+    private void initialise(String code){
         welcomeInfo = (TextView) findViewById(R.id.welcomeTxt);
         welcomeInfo.setText("Welcome "+message+"!");
+
+        ImageView imageView = (ImageView) findViewById(R.id.qrCode);
+        try {
+            Bitmap bitmap = encodeAsBitmap(code);
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
+
+
+
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, 500, 500, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, 500, 0, 0, w, h);
+        return bitmap;
+    }
+
     public void backToLogin(View view) {
        finish();
     }
