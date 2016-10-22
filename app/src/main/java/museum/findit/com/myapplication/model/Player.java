60192df273759743c5a3350c609e3c1067ecf4b9
Doni Ramadhan <doniramadhan@gmail.com>
2016-10-23 01:53:20 +0200

    add listener for gameboard

---
+++ app/src/main/java/museum/findit/com/myapplication/model/Player.java
@@ -0,0 +1,12 @@
+package museum.findit.com.myapplication.model;
+
+/**
+ * Created by doniramadhan on 2016-10-23.
+ */
+public class Player {
+    public Integer order;
+    public Integer percentage;
+    public Integer score;
+    public String username;
+
+}
--- app/src/main/java/museum/findit/com/myapplication/view/fragment/LeaderboardFragment.java
+++ app/src/main/java/museum/findit/com/myapplication/view/fragment/LeaderboardFragment.java
@@ -1,13 +1,24 @@
 package museum.findit.com.myapplication.view.fragment;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import museum.findit.com.myapplication.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import museum.findit.com.myapplication.R;
+import museum.findit.com.myapplication.WebService.GameService;
+import museum.findit.com.myapplication.model.Player;
 
 
 /**
@@ -20,5 +31,27 @@ public class LeaderboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_leaderboard, container, false);
 
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        GameService.listenScoreboard(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<HashMap<String, Player>> objectListType = new GenericTypeIndicator<HashMap<String, Player>>() {};
+                HashMap<String, Player> players = dataSnapshot.getValue(objectListType);
+                for (Map.Entry<String, Player> entry: players.entrySet()) {
+                    Player player = entry.getValue();
+                    // TODO: 2016-10-23 use player to build scoreboard
+                    Log.d("GameLeaderBoard", player.username + "|" + player.score.toString() + "|" + player.percentage.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("GameLog", "Players cannot be read: " + databaseError.getDetails());
+            }
+        });
     }
 }
