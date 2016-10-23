7c93bf7b577709a275faf0a9eec7567184c709c4
Doni Ramadhan <doniramadhan@gmail.com>
2016-10-23 15:27:10 +0200

    create leaderboard table programmatically

--- app/src/main/java/museum/findit/com/myapplication/view/fragment/LeaderboardFragment.java
+++ app/src/main/java/museum/findit/com/myapplication/view/fragment/LeaderboardFragment.java
@@ -1,12 +1,19 @@
 package museum.findit.com.myapplication.view.fragment;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,6 +34,8 @@ import museum.findit.com.myapplication.model.Player;
 
 public class LeaderboardFragment extends Fragment {
 
+    private Boolean isLeaderboardCreated = false;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_leaderboard, container, false);
@@ -42,6 +51,10 @@ public class LeaderboardFragment extends Fragment {
                 GenericTypeIndicator<HashMap<String, Player>> objectListType = new GenericTypeIndicator<HashMap<String, Player>>() {};
                 HashMap<String, Player> players = dataSnapshot.getValue(objectListType);
                 if(players == null) return;
+                if(!isLeaderboardCreated){
+                    createLeaderboard();
+                    isLeaderboardCreated = true;
+                }
                 for (Map.Entry<String, Player> entry: players.entrySet()) {
                     Player player = entry.getValue();
                     // TODO: 2016-10-23 use player to build scoreboard
@@ -54,5 +67,70 @@ public class LeaderboardFragment extends Fragment {
                 Log.d("GameLog", "Players cannot be read: " + databaseError.getDetails());
             }
         });
+    }
+
+    private void createLeaderboard(){
+        Context context = getContext();
+        TableLayout leaderboardLayout = (TableLayout) getActivity().findViewById(R.id.leaderboard);
+
+        addTableRow(context, leaderboardLayout, "Bob", 120, 30);
+        addTableRow(context, leaderboardLayout, "You", 120, 30);
+        addTableRow(context, leaderboardLayout, "Me", 120, 30);
+    }
+
+    private HashMap<String, TextView> addTableRow(Context context, TableLayout leaderboardLayout, String username, Integer score, Integer percentage) {
+        TableRow row = new TableRow(context);
+        TableRow.LayoutParams tableRowParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.MATCH_PARENT, 1.0f);
+        row.setLayoutParams(tableRowParams);
+        leaderboardLayout.addView(row);
+
+        TextView usernameTextView = addUsernameTextView(context, row, username);
+        TextView scoreTextView = addNumberTextView(context, row, score);
+        TextView percentageTextView = addNumberTextView(context, row, percentage);
+
+        HashMap<String, TextView> textViewHashMap = new HashMap<>();
+        textViewHashMap.put("username", usernameTextView);
+        textViewHashMap.put("score", scoreTextView);
+        textViewHashMap.put("percentage", percentageTextView);
+        return textViewHashMap;
+    }
+
+    private TextView addUsernameTextView(Context context, TableRow row, String username) {
+        TextView usernameTextView = new TextView(context);
+
+        TableRow.LayoutParams textViewParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT, 1.0f);
+        textViewParams.topMargin = 30;
+        usernameTextView.setLayoutParams(textViewParams);
+
+        usernameTextView.setTextColor(Color.parseColor("#ff669900"));
+        usernameTextView.setTextSize(18);
+        usernameTextView.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        usernameTextView.setFocusable(true);
+        usernameTextView.setFocusableInTouchMode(true);
+        usernameTextView.setGravity(Gravity.CENTER);
+        usernameTextView.setText(username);
+        row.addView(usernameTextView);
+        return usernameTextView;
+    }
+
+    private TextView addNumberTextView(Context context, TableRow row, Integer number) {
+        TextView numberTextView = new TextView(context);
+
+        TableRow.LayoutParams textViewParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT, 1.0f);
+        textViewParams.topMargin = 30;
+        numberTextView.setLayoutParams(textViewParams);
+
+        numberTextView.setTextColor(Color.BLACK);
+        numberTextView.setTextSize(18);
+        numberTextView.setGravity(Gravity.CENTER);
+        numberTextView.setText(number.toString());
+        row.addView(numberTextView);
+        return numberTextView;
     }
 }
